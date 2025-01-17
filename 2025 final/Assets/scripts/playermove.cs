3c4799532a6c3f3372dd7415790d8f25faada13d
Morbius <113389499+Dr-m-morbius@users.noreply.github.com>
2025-01-17 09:21:49 -0800

    rahhhhh

--- 2025 final/Assets/scripts/playermove.cs
+++ 2025 final/Assets/scripts/playermove.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class playermove : MonoBehaviour
 {
+    public float moveSpeed = 1f;
+    public Transform theCamera;
+       private Vector3 _moveInput;
+         public float mouseSensitivity = 1f;
+       private CharacterController _characterController;
     // Start is called before the first frame update
     void Start()
     {
-        
+         _characterController = GetComponent<CharacterController>();
+         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //playermovement
+          Vector3 forwardDirection = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horizontalDirection = transform.right * Input.GetAxis("Horizontal");
+
+        _moveInput = (forwardDirection + horizontalDirection).normalized;
+        _moveInput *= moveSpeed;
+        _characterController.Move(_moveInput * Time.deltaTime);
+
+        //camrea 
+         Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
+
+        theCamera.rotation = Quaternion.Euler(theCamera.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
     }
 }
