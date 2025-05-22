a329ae7ef74831bb48a961151d30002e92e12e7e
Morbius <113389499+Dr-m-morbius@users.noreply.github.com>
2025-05-22 09:16:36 -0700

    trying

--- 2025 final/Assets/scripts/playermove.cs
+++ 2025 final/Assets/scripts/playermove.cs
@@ -8,14 +8,14 @@ using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UnityEngine.SceneManagement; 
+using UnityEngine.SceneManagement;
 
 
 
 public class playermove : MonoBehaviour
-{     
+{
     public float maxSlope;
-        
+
     private RaycastHit slopehit;
     public float crouchspeed;
     public float crouchyscale;
@@ -26,7 +26,7 @@ public class playermove : MonoBehaviour
     public KeyCode crouchkey = KeyCode.LeftControl;
     float horizontalinput;
     float verticalinput;
-     public float Gravity = 1f;
+    public float Gravity = 1f;
     public float groundDrag;
     public ParticleSystem jumpps;
     public float jumpforce = 5;
@@ -41,10 +41,11 @@ public class playermove : MonoBehaviour
         crouching
     }
     public float jumpcooldown = 5f;
-public float airmultiplier;
-public bool canjump = true;
+    public float airmultiplier;
+    public bool canjump = true;
     public float playerheight;
     public LayerMask whatisground;
+    public LayerMask whatiswall;
     public bool grounded;
 
     Vector3 movedirection;
@@ -52,21 +53,23 @@ public bool canjump = true;
     public float walkspeed;
     public float runspeed;
     public KeyCode sprintkey = KeyCode.LeftShift;
-       private Vector3 _moveInput;
-       private KeyCode jumpkey = KeyCode.Space;
-       public Transform orentation;
-       private Rigidbody rb;
+    private Vector3 _moveInput;
+    public bool walljumpL = false;
+    public bool walljumpR = false;
+    private KeyCode jumpkey = KeyCode.Space;
+    public Transform orentation;
+    private Rigidbody rb;
 
-       
+
 
     // Start is called before the first frame update
 
-    
+
     void Start()
     {
-         Physics.gravity *= Gravity;
-         canjump = true;
-         source = GetComponent<AudioSource>();
+        Physics.gravity *= Gravity;
+        canjump = true;
+        source = GetComponent<AudioSource>();
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
         startyscale = transform.localScale.y;
@@ -74,76 +77,80 @@ public bool canjump = true;
 
     }
 
-        void Update()
+    void Update()
     {
-        input(); 
+        
+        input();
         statehandler();
         if (transform.position.y <= -30)
         {
-             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
 
         grounded = Physics.Raycast(transform.position, Vector3.down, playerheight * 0.5f + 0.2f, whatisground);
-
+        walljumpL = Physics.Raycast(transform.position, Vector3.left, playerheight * 1f + 0.5f, whatiswall);
+        walljumpR = Physics.Raycast(transform.position, Vector3.right, playerheight * 1f + 0.5f, whatiswall);
         //drag stuffs 
-        if(grounded)
+        if (grounded)
         {
             rb.drag = groundDrag;
-            
+
         }
-        else 
+        else
         {
             rb.drag = 0;
         }
-       //speedcontrol();
+        //speedcontrol();
     }
-      private void FixedUpdate()
+    private void FixedUpdate()
     {
-         movement();
+        movement();
+
     }
 
 
     private void input()
     {
         horizontalinput = Input.GetAxisRaw("Horizontal");
-        verticalinput = Input.GetAxisRaw("Vertical"); 
+        verticalinput = Input.GetAxisRaw("Vertical");
 
-        if(Input.GetKey(jumpkey) && canjump && grounded)
+        if (Input.GetKey(jumpkey) && canjump && grounded || walljumpL || walljumpR)
         {
             canjump = false;
             jump();
             Invoke(nameof(resetjump), jumpcooldown);
             source.PlayOneShot(jumping);
         }
+    
 
-    //start crouch
+            //start crouch
 
-    if (Input.GetKeyDown(crouchkey))
-    {
-        transform.localScale = new Vector3(transform.localScale.x, crouchyscale,transform.localScale.z);
-        rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
-    }
-    //stop crouch
-    if (Input.GetKeyUp(crouchkey))
-    {
-        transform.localScale = new Vector3(transform.localScale.x, startyscale, transform.localScale.z);
-    }
-    }
+            if (Input.GetKeyDown(crouchkey))
+            {
+                transform.localScale = new Vector3(transform.localScale.x, crouchyscale, transform.localScale.z);
+                rb.AddForce(Vector3.down * 5f, ForceMode.Impulse);
+            }
+            //stop crouch
+            if (Input.GetKeyUp(crouchkey))
+            {
+                transform.localScale = new Vector3(transform.localScale.x, startyscale, transform.localScale.z);
+            }
+        } 
 
     private void statehandler()
     {
         //put in sprint state
-        if(grounded && Input.GetKey(sprintkey))
+        if (grounded && Input.GetKey(sprintkey))
         {
             state = Movementstate.running;
             moveSpeed = runspeed;
         }
         //put in walking state
-        else if(grounded)
+        else if (grounded)
         {
             state = Movementstate.walkking;
             moveSpeed = walkspeed;
-            
+
 
         }
 
@@ -165,27 +172,27 @@ public bool canjump = true;
         }
     }
 
-  
+
     private void movement()
     {
-        movedirection = orentation.forward * verticalinput + orentation.right *horizontalinput;
+        movedirection = orentation.forward * verticalinput + orentation.right * horizontalinput;
 
         //on slope
-        if(OnSlope())
+        if (OnSlope())
         {
             rb.AddForce(Slopedirection() * moveSpeed * 10f, ForceMode.Force);
         }
-            //on ground
-            if(grounded)
-            {
-        rb.AddForce(movedirection.normalized * moveSpeed * 10f, ForceMode.Force);
-            }
+        //on ground
+        if (grounded)
+        {
+            rb.AddForce(movedirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
         //in air
-        else if(!grounded)
+        else if (!grounded)
         {
             rb.AddForce(movedirection.normalized * moveSpeed * 10f * airmultiplier, ForceMode.Force);
         }
-        
+
     }
 
     private void speedcontrol()
@@ -195,37 +202,44 @@ public bool canjump = true;
 
         //limit velocity
 
-        if(flatVel.magnitude > moveSpeed)
+        if (flatVel.magnitude > moveSpeed)
         {
-            Vector3 LimitedVel  = flatVel.normalized * moveSpeed;
+            Vector3 LimitedVel = flatVel.normalized * moveSpeed;
             rb.velocity = new Vector3(LimitedVel.x, rb.velocity.y, LimitedVel.z);
         }
     }
 
     private void jump()
     {
-     //reset y velocity for always jumping same height
-     rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        //reset y velocity for always jumping same height
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-     rb.AddForce(transform.up * jumpforce, ForceMode.Impulse); 
+        rb.AddForce(transform.up * jumpforce, ForceMode.Impulse);
     }
-   private void resetjump()
-   {
-    canjump = true;
-   }
-   private bool OnSlope()
-   {
-    if(Physics.Raycast(transform.position, Vector3.down, out slopehit, playerheight * 0.5f + 0.3f))
+    private void resetjump()
     {
-        float angle = Vector3.Angle(Vector3.up, slopehit.normal);
-        return angle < maxSlope && angle != 0;
+        canjump = true;
+    }
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopehit, playerheight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopehit.normal);
+            return angle < maxSlope && angle != 0;
+        }
+
+        return false;
     }
 
-    return false;
-   }
-
-   private Vector3 Slopedirection()
-   {
-    return Vector3.ProjectOnPlane(movedirection, slopehit.normal).normalized;
-   }
+    private Vector3 Slopedirection()
+    {
+        return Vector3.ProjectOnPlane(movedirection, slopehit.normal).normalized;
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("wall"))
+        {
+            
+        }
+    }
 }
